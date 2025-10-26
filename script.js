1ccf76cc5184410288d90966b505ce74479317f7
aswathamaallu007-star <aswathamaallu786@gmail.com>
2025-10-26 10:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+setInterval(()=>{
+	const currentTime = new Date()
+    const p = document.getElementById('timer')
+	p.innerText = ` ${currentTime.toLocaleDateString()}, ${currentTime.toLocaleTimeString()}`
+	
+},1000)
